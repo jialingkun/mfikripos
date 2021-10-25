04cb92fc972c49f2c20cd0bfedd7a6ebd0b46b26
Enny Hartz Tono <djaka.tingkir2@gmail.com>
2021-10-25 18:21:39 +0700

    bug fix cart limit cuma 6

--- application/views/admin/v_penjualan_grosir.php
+++ application/views/admin/v_penjualan_grosir.php
@@ -211,8 +211,8 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="<?php echo base_url().'assets/dist/js/bootstrap-select.min.js'?>"></script>
     <script src="<?php echo base_url().'assets/js/bootstrap.min.js'?>"></script>
-    <script src="<?php echo base_url().'assets/js/dataTables.bootstrap.min.js'?>"></script>
     <script src="<?php echo base_url().'assets/js/jquery.dataTables.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/js/dataTables.bootstrap.min.js'?>"></script>
     <script src="<?php echo base_url().'assets/js/jquery.price_format.min.js'?>"></script>
     <script src="<?php echo base_url().'assets/js/moment.js'?>"></script>
     <script src="<?php echo base_url().'assets/js/bootstrap-datetimepicker.min.js'?>"></script>
--- db_penjualan.sql
+++ db_penjualan.sql
@@ -23,6 +23,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `ci_sessions`
+--
+CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+    session_id varchar(40) DEFAULT '0' NOT NULL,
+    ip_address varchar(45) DEFAULT '0' NOT NULL,
+    user_agent varchar(120) NOT NULL,
+    last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+    user_data text NOT NULL,
+    PRIMARY KEY (session_id),
+    KEY `last_activity_idx` (`last_activity`)
+);
+
+
+--
 -- Struktur dari tabel `tbl_barang`
 --
 
